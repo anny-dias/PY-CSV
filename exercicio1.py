d92948d112a68a141b38d9c3188d61d689553328
Logon Aluno <logonrmlocal@fiap.com.br>
2023-10-10 09:19:30 -0300

    py

---
+++ exercicio1.py
@@ -0,0 +1,11 @@
+'''Exercício 1 
+Considere que o arquivo “notas.CSV” apresenta uma listagem com os dados dos alunos de uma turma. 
+Cada linha do arquivo apresenta os dados de um aluno, no formato: 
+RM,NOME,NOTA1,NOTA2,NOTA3,NOTA4
+Implemente um programa que leia o arquivo indicado e, a partir desse arquivo, gere dois novos 
+arquivos:
+• Arquivo “aprovados.CSV” contendo uma listagem dos alunos aprovados na disciplina, contendo 
+RM,NOME,MEDIA do aluno 
+• Arquivo “reprovados.CSV” contendo uma listagem dos nomes dos alunos reprovados na 
+disciplina, contendo RM,NOME,MEDIA do aluno. 
+Para o aluno ser aprovado ele deve ter média igual ou superior a 6.0'''
---
+++ exercicio2.py
@@ -0,0 +1,5 @@
+'''Exercício 2 
+Considere o arquivo “foods.CSV”, com três colunas: "name" (nome), "id" (identificação) e "favorite food" 
+(comida favorita).
+Cada linha representa um indivíduo com suas respectivas informações. 
+Escreve um programa que leia esse arquivo e informe a comida preferida pela maioria das pessoas.'''
